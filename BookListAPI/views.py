40ab6f1b95f5c11be584318586bd588a9c7a9b5e
David Kelechi Anosike <Kelechid47@gmail.com>
2026-02-28 09:42:17 +0100

    Complete get post put and update actions

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+#from .models import Book
 
 # Create your views here.
+@api_view(['GET','POST'])
+def books(request):
+    return Response('list of the books', status=status.HTTP_200_OK)
+
+class BookList(APIView): 
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message":"list of the books by " + author}, status.HTTP_200_OK)
+        
+        return Response({"message": "list of the books"}, status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({"title": request.data.get('title')}, status=status.HTTP_201_CREATED)
+    
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"Message": "Single book with ID " + str(pk)}, status=status.HTTP_200_OK)
+    
+    def put(self, request):
+        return Response({"title": request.data.get('title')}, status=status.HTTP_200_OK)
+
+    
